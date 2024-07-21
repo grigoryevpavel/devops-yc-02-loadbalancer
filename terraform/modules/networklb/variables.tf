399b283530c877bbfe02b789500712cffc2280a9
grigoryevpavel <grpa@mail.ru>
2024-07-21 16:51:05 +0000

    init

---
+++ terraform/modules/networklb/variables.tf
@@ -0,0 +1,13 @@
+ 
+
+variable "vmg_id" {
+  type    = string
+  default = null 
+  description = "идентификатор группы ВМ"
+}
+
+variable "lb_name" {
+  type    = string
+  default = "lb-lamp" 
+  description = "имя баланировщика"
+}  
---
+++ terraform/modules/vmgroup/permission_accounts.tf
@@ -0,0 +1,12 @@
+# Создаем сервисный аккаунт
+resource "yandex_iam_service_account" "sa" {
+  folder_id = var.folder_id
+  name      = var.account_name
+}
+ 
+// Предоставляем права администратора аккаунту sa
+resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
+  folder_id = var.folder_id
+  role      = "admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
